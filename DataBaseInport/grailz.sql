3e9b11bbe44630ceb7d11068e38d97f00ba53344
fzcnbch0 <bruno.szwec@uczen.zsk.poznan.pl>
2024-06-10 13:15:10 +0200

    db import

--- DataBaseInport/grailz.sql
+++ DataBaseInport/grailz.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 31 Maj 2024, 21:24
--- Wersja serwera: 10.4.25-MariaDB
--- Wersja PHP: 8.1.10
+-- Generation Time: Cze 10, 2024 at 01:14 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `grailz`
+-- Database: `grailz`
 --
 
 -- --------------------------------------------------------
@@ -32,26 +32,26 @@ CREATE TABLE `item` (
   `name` varchar(100) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `item`
+-- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`item_id`, `name`, `description`, `price`) VALUES
-(1, 'black-levis', 'Classic black Levis jeans for everyday wear.', '19.99'),
-(2, 'black-pants', 'Classic black denim jeans with a comfortable fit.', '49.99'),
-(3, 'black-print-tshirt', 'Stylish black printed t-shirt for a trendy look.', '199.99'),
-(4, 'black-tshirt', 'Simple black t-shirt for casual wear.', '29.99'),
-(5, 'blue-jeans', 'Comfortable blue jeans for everyday wear.', '39.99'),
-(6, 'boxy-tshirt', 'Loose-fitting boxy t-shirt for a relaxed look.', '149.99'),
-(7, 'camo-jacket', 'Trendy camo jacket for a rugged outdoor style.', '59.99'),
-(8, 'camo-pants', 'Camouflage print pants for a military-inspired look.', '14.99'),
-(9, 'double-knne-pants', 'Stylish double-knee pants for added durability.', '24.99'),
-(10, 'vintage-jacket', 'Vintage-style jacket for a retro look.', '69.99'),
-(14, 'white-hat', 'Classic white hat to complement any outfit.', '39.99'),
-(15, 'white-navy-hat', 'Stylish white and navy hat for a modern look.', '59.99'),
-(16, 'white-tshirt', 'Simple white t-shirt for everyday wear.', '79.99');
+(1, 'black-levis', 'Classic black Levis jeans for everyday wear.', 19.99),
+(2, 'black-pants', 'Classic black denim jeans with a comfortable fit.', 49.99),
+(3, 'black-print-tshirt', 'Stylish black printed t-shirt for a trendy look.', 199.99),
+(4, 'black-tshirt', 'Simple black t-shirt for casual wear.', 29.99),
+(5, 'blue-jeans', 'Comfortable blue jeans for everyday wear.', 39.99),
+(6, 'boxy-tshirt', 'Loose-fitting boxy t-shirt for a relaxed look.', 149.99),
+(7, 'camo-jacket', 'Trendy camo jacket for a rugged outdoor style.', 59.99),
+(8, 'camo-pants', 'Camouflage print pants for a military-inspired look.', 14.99),
+(9, 'double-knne-pants', 'Stylish double-knee pants for added durability.', 24.99),
+(10, 'vintage-jacket', 'Vintage-style jacket for a retro look.', 69.99),
+(14, 'white-hat', 'Classic white hat to complement any outfit.', 39.99),
+(15, 'white-navy-hat', 'Stylish white and navy hat for a modern look.', 59.99),
+(16, 'white-tshirt', 'Simple white t-shirt for everyday wear.', 79.99);
 
 -- --------------------------------------------------------
 
@@ -65,26 +65,26 @@ CREATE TABLE `item_category` (
   `category` varchar(100) DEFAULT NULL,
   `size` varchar(10) DEFAULT NULL,
   `designer` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `item_category`
+-- Dumping data for table `item_category`
 --
 
 INSERT INTO `item_category` (`item_id`, `department`, `category`, `size`, `designer`) VALUES
-(1, 'Men', 'Tops', 'L', 'Affliction'),
+(1, 'Men', 'Bottoms', 'L', 'Affliction'),
 (2, 'Women', 'Bottoms', 'M', 'Vintage'),
-(3, 'Men', 'Outerwear', 'XL', 'Y2K'),
-(4, 'Women', 'Dresses', 'M', 'Affliction'),
+(3, 'Men', 'Tops', 'XL', 'Y2K'),
+(4, 'Women', 'Tops', 'M', 'Affliction'),
 (5, 'Men', 'Bottoms', 'L', 'Vintage'),
-(6, 'Men', 'Outerwear', 'L', 'Vintage'),
-(7, 'Unisex', 'Footwear', '10', 'Vintage'),
-(8, 'Unisex', 'Accessories', 'One Size', 'Affliction'),
-(9, 'Unisex', 'Accessories', 'One Size', 'Y2K'),
-(10, 'Unisex', 'Footwear', '9', 'Vintage'),
-(14, 'Men', 'Tops', 'L', 'Affliction'),
-(15, 'Women', 'Bottoms', 'M', 'Vintage'),
-(16, 'Men', 'Outerwear', 'M', 'Y2K');
+(6, 'Men', 'Tops', 'L', 'Vintage'),
+(7, 'women', 'tops', '10', 'Vintage'),
+(8, 'women', 'bottoms', 'One Size', 'Affliction'),
+(9, 'men', 'bottoms', 'One Size', 'Y2K'),
+(10, 'women', 'tops', '9', 'Vintage'),
+(14, 'Men', 'accessories', 'L', 'Affliction'),
+(15, 'Women', 'accessories', 'M', 'Vintage'),
+(16, 'Men', 'tops', 'M', 'Y2K');
 
 -- --------------------------------------------------------
 
@@ -96,10 +96,10 @@ CREATE TABLE `measurements` (
   `item_id` int(11) NOT NULL,
   `length` float DEFAULT NULL,
   `width` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `measurements`
+-- Dumping data for table `measurements`
 --
 
 INSERT INTO `measurements` (`item_id`, `length`, `width`) VALUES
@@ -126,10 +126,10 @@ INSERT INTO `measurements` (`item_id`, `length`, `width`) VALUES
 CREATE TABLE `offer` (
   `item_id` int(11) NOT NULL,
   `image_path` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `offer`
+-- Dumping data for table `offer`
 --
 
 INSERT INTO `offer` (`item_id`, `image_path`) VALUES
@@ -159,30 +159,25 @@ CREATE TABLE `user` (
   `balance` decimal(10,2) DEFAULT NULL,
   `city` varchar(100) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `name`, `balance`, `city`, `password`) VALUES
-(1, 'John Doe', '100.00', 'New York', 'password1'),
-(2, 'Jane Smith', '150.50', 'Los Angeles', 'password2'),
-(3, 'Alice Johnson', '75.25', 'Chicago', 'password3'),
-(4, 'Bob Brown', '50.00', 'Houston', 'password4'),
-(5, 'Charlie Davis', '200.00', 'Phoenix', 'password5'),
-(6, 'Diana Evans', '300.75', 'Philadelphia', 'password6'),
-(7, 'Frank Green', '125.00', 'San Antonio', 'password7'),
-(8, 'Grace Harris', '80.00', 'San Diego', 'password8'),
-(9, 'Henry Irwin', '90.50', 'Dallas', 'password9'),
-(10, 'Ivy Johnson', '120.00', 'San Jose', 'password10'),
-(11, 'John Doe', NULL, 'New York', '$2a$10$rNlov4cfckGiyp8RLzmwcuUyzaXPpmrktszX5ITHqclifnjUrfAN6'),
-(12, 'John Doe2', NULL, 'New York', '$2a$10$kDY9UMgg1X6Kt5S1HgbPBeTWhk25Me2AMehqaRBceRltcw/QDJXf2'),
-(13, 'John Doe2', NULL, 'New York', '$2a$10$j59CvzWR2cULcYIemd/Y6uY0dOinRgZzBjmYGrIZkDYoONKehTEES'),
-(14, 'John Doe2', NULL, 'neww', '$2a$10$Ixy5f/0fQWx0XPgJQxaw0.15khFpwblQCGp8oOug5KeECHatgtvSO'),
-(15, 'John Doe11', NULL, NULL, 'password2'),
+(1, 'Bruno Szwec', 100.00, 'New York', 'password1'),
+(2, 'Jane Smith', 150.50, 'Los Angeles', 'password2'),
+(3, 'Alice Johnson', 75.25, 'Chicago', 'password3'),
+(4, 'Bob Brown', 50.00, 'Houston', 'password4'),
+(5, 'Charlie Davis', 200.00, 'Phoenix', 'password5'),
+(6, 'Diana Evans', 300.75, 'Philadelphia', 'password6'),
+(7, 'Frank Green', 125.00, 'San Antonio', 'password7'),
+(8, 'Grace Harris', 80.00, 'San Diego', 'password8'),
+(9, 'Henry Irwin', 90.50, 'Dallas', 'password9'),
+(10, 'Ivy Johnson', 120.00, 'San Jose', 'password10'),
 (16, '1', NULL, NULL, '1'),
-(17, 'bruno', NULL, 'poznan', 'szwec');
+(18, '3', NULL, '3', '3');
 
 -- --------------------------------------------------------
 
@@ -193,16 +188,13 @@ INSERT INTO `user` (`user_id`, `name`, `balance`, `city`, `password`) VALUES
 CREATE TABLE `user_cart` (
   `user_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `user_cart`
+-- Dumping data for table `user_cart`
 --
 
 INSERT INTO `user_cart` (`user_id`, `item_id`) VALUES
-(1, 2),
-(1, 3),
-(1, 14),
 (2, 1),
 (2, 4),
 (2, 15),
@@ -223,7 +215,13 @@ INSERT INTO `user_cart` (`user_id`, `item_id`) VALUES
 (9, 8),
 (10, 9),
 (10, 10),
-(16, 3);
+(16, 1),
+(16, 3),
+(16, 4),
+(16, 5),
+(16, 6),
+(16, 8),
+(18, 2);
 
 -- --------------------------------------------------------
 
@@ -235,31 +233,51 @@ CREATE TABLE `user_orders` (
   `user_id` int(11) DEFAULT NULL,
   `order_id` int(11) NOT NULL,
   `item_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `user_orders`
+-- Dumping data for table `user_orders`
 --
 
 INSERT INTO `user_orders` (`user_id`, `order_id`, `item_id`) VALUES
-(1, 1, 1),
-(2, 2, 2),
-(3, 3, 3),
-(4, 4, 4),
-(5, 5, 5),
-(6, 6, 6),
-(7, 7, 7),
-(8, 8, 8),
-(9, 9, 9),
-(10, 10, 10),
-(1, 11, 2),
-(2, 12, 3),
-(3, 13, 4),
-(4, 14, 5),
-(5, 15, 6),
-(1, 16, 14),
-(2, 17, 15),
-(3, 18, 16);
+(1, 26, 6),
+(1, 27, 4),
+(1, 28, 4),
+(1, 29, 1),
+(1, 30, 3),
+(1, 31, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user_shipping`
+--
+
+CREATE TABLE `user_shipping` (
+  `user_id` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(20) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_shipping`
+--
+
+INSERT INTO `user_shipping` (`user_id`, `address`, `city`, `state`, `postal_code`, `country`, `phone_number`) VALUES
+(1, 'Limonkowa 6/2', 'Poznań', 'Wielkopolska', '10001', 'Poland', '665 660 112'),
+(2, '456 Maple Ave', 'Los Angeles', 'CA', '90001', 'USA', '555-5678'),
+(3, '789 Oak Dr', 'Chicago', 'IL', '60601', 'USA', '555-8765'),
+(4, '101 Pine St', 'Houston', 'TX', '77001', 'USA', '555-4321'),
+(5, '202 Birch Rd', 'Phoenix', 'AZ', '85001', 'USA', '555-3456'),
+(6, '303 Cedar Ln', 'Philadelphia', 'PA', '19101', 'USA', '555-6543'),
+(7, '404 Spruce Ct', 'San Antonio', 'TX', '78201', 'USA', '555-9876'),
+(8, '505 Willow Blvd', 'San Diego', 'CA', '92101', 'USA', '555-7654'),
+(9, '606 Elm St', 'Dallas', 'TX', '75201', 'USA', '555-8764'),
+(10, '707 Fir St', 'San Jose', 'CA', '95101', 'USA', '555-6542');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -311,62 +329,74 @@ ALTER TABLE `user_orders`
   ADD KEY `item_id` (`item_id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- Indeksy dla tabeli `user_shipping`
+--
+ALTER TABLE `user_shipping`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `item`
+-- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
   MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT dla tabeli `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT dla tabeli `user_orders`
+-- AUTO_INCREMENT for table `user_orders`
 --
 ALTER TABLE `user_orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `item_category`
+-- Constraints for table `item_category`
 --
 ALTER TABLE `item_category`
   ADD CONSTRAINT `item_category_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
 
 --
--- Ograniczenia dla tabeli `measurements`
+-- Constraints for table `measurements`
 --
 ALTER TABLE `measurements`
   ADD CONSTRAINT `measurements_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
 
 --
--- Ograniczenia dla tabeli `offer`
+-- Constraints for table `offer`
 --
 ALTER TABLE `offer`
   ADD CONSTRAINT `offer_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
 
 --
--- Ograniczenia dla tabeli `user_cart`
+-- Constraints for table `user_cart`
 --
 ALTER TABLE `user_cart`
   ADD CONSTRAINT `user_cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `user_cart_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
 
 --
--- Ograniczenia dla tabeli `user_orders`
+-- Constraints for table `user_orders`
 --
 ALTER TABLE `user_orders`
   ADD CONSTRAINT `user_orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `user_orders_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+
+--
+-- Constraints for table `user_shipping`
+--
+ALTER TABLE `user_shipping`
+  ADD CONSTRAINT `user_shipping_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
